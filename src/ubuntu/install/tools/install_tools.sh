777e1a4b464e2e1b0250972ad7d30c73a33946be
Andrés Calimero <andrescalimero@hotmail.es>
2023-05-06 17:44:39 +0000

    feat: add base image

---
+++ src/ubuntu/install/tools/install_tools.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+set -e
+
+echo "Install some common tools for further installation"
+if [[ "${DISTRO}" == @(centos|oracle7) ]] ; then
+  yum install -y vim wget net-tools bzip2 python3 ca-certificates
+elif [[ "${DISTRO}" == @(fedora37|oracle8|oracle9|rockylinux9|rockylinux8|almalinux8|almalinux9) ]]; then
+  dnf install -y wget net-tools bzip2 python3 tar vim hostname procps-ng
+elif [ "${DISTRO}" == "opensuse" ]; then
+  sed -i 's/download.opensuse.org/mirrorcache-us.opensuse.org/g' /etc/zypp/repos.d/*.repo
+  zypper install -yn wget net-tools bzip2 python3 tar vim gzip iputils
+elif [ "${DISTRO}" == "alpine" ]; then
+  apk add --no-cache \
+    ca-certificates \
+    curl \
+    gcompat \
+    grep \
+    libgcc \
+    mcookie \
+    net-tools \
+    openssh-client \
+    openssl \
+    shadow \
+    sudo \
+    tar \
+    wget
+else
+  apt-get update
+  # Update tzdata noninteractive (otherwise our script is hung on user input later).
+  DEBIAN_FRONTEND=noninteractive TZ=Etc/UTC apt-get -y install tzdata
+  apt-get install -y vim wget net-tools locales bzip2 wmctrl software-properties-common mesa-utils
+
+  echo "generate locales for en_US.UTF-8"
+  locale-gen en_US.UTF-8
+fi
+
+if [ "$DISTRO" = "ubuntu" ]; then
+  #update mesa to latest
+  add-apt-repository ppa:kisak/turtle
+  apt-get update
+  apt full-upgrade -y
+fi
